db18beb764234d3f89e502ec799231bb0340b664
thismatters <paul@thismatters.net>
2018-10-03 19:00:28 -0500

    some notes

--- workouts/admin.py
+++ workouts/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Workout
+from workouts.models import Workout, Exercise
+### PS: always better to use the full reference for import
+
 admin.site.register(Workout)
+admin.site.register(Exercise)
--- workouts/models.py
+++ workouts/models.py
@@ -3,19 +3,31 @@ from django.utils import timezone
 
 # Create your models here.
 class Workout(models.Model):
-    lifter = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    exercise = models.CharField(max_length=50)
-    weight = models.CharField(max_length=4)
+    lifter = models.ForeignKey('auth.User', on_delete=models.CASCADE, 
+        related_name='workouts')
+    # exercise = models.CharField(max_length=50)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE, 
+        related_name='workouts')
+    weight = models.CharField(max_length=4) ### PS: porque no los IntegerField???
     reps_Set_1 = models.IntegerField(default=0)
     reps_Set_2 = models.IntegerField(default=0)
     reps_Set_3 = models.IntegerField(default=0)
     lifted_date = models.DateTimeField(
-        blank=True, null=True)
+        blank=True, null=True, auto_now_add=True)
+    ### PS: consider using `auto_now_add=True` in that DateTimeField rather than using the publish function to set the date to now. 
     notes = models.TextField()
 
-    def publish(self):
-        self.lifted_date = timezone.now()
-        self.save()
+    # def publish(self):
+    #     self.lifted_date = timezone.now()
+    #     self.save()
 
     def __str__(self):
-        return self.exercise
+        return f'{selflifter} -- {self.exercise} -- {self.lifted_date}'
+
+
+class Exercise(models.Model):
+    """Keep track of the supported exercises. Allows for generation of
+        workouts and reporting on dem gainz"""
+    name = models.CharField(max_length=50)
+    # eventually the muscle_group can be its own model as well to further refine
+    muscle_group = models.CharField(max_length=90)
