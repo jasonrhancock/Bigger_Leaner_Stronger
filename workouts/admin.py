563482873c07d50ec80d1bc4eb315cb21033e800
Paul Stiverson <thismatters@users.noreply.github.com>
2018-10-09 09:15:37 -0500

    Update admin.py

--- workouts/admin.py
+++ workouts/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
-from workouts.models import Workout, Exercise
+from workouts.models import Workout, Exercise, Set, ExerciseType
 ### PS: always better to use the full reference for import
 
-admin.site.register(Workout)
+class SetInline(admin.TabularInline):
+    model = Set
+
+class ExerciseInline(admin.TabularInline):
+    model = Exercise
+    inlines = [SetInline]
+
+class WorkoutAdmin(admin.ModelAdmin):
+    model = Workout
+    fields = ('lifter',)
+    readonly_fields = ('date',)
+    inlines = [ExerciseInline]
+
+admin.site.register(Workout, WorkoutAdmin)
 admin.site.register(Exercise)
+admin.site.register(ExerciseType)
