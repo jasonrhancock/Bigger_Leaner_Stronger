54955cdd6197d9d78a9452a9b17c020d03a5dca2
Paul Stiverson <thismatters@users.noreply.github.com>
2018-10-09 08:33:36 -0500

    Update models.py
    no... like this dingus

--- workouts/models.py
+++ workouts/models.py
@@ -2,24 +2,29 @@ from django.db import models
 from django.core.validators import MaxValueValidator
 from django import forms
 
-# Create your models here.
+
+# Workout
+    # Exercise
+        # Set
 class Workout(models.Model):
     lifter = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='workouts')
-    exercise = models.ForeignKey('Exercise', on_delete=models.CASCADE)
-    set_1_Weight = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(1500)])
-    set_1_Reps = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(50)])
-    set_2_Weight = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(1500)])
-    set_2_Reps = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(50)])
-    set_3_Weight = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(1500)])
-    set_3_Reps = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(50)])
-    lifted_date = models.DateTimeField(blank=True, null=True, auto_now_add=True)
+    date = models.DateTimeField(blank=True, null=True, auto_now_add=True)
+    
+# Create your models here.
+class Exercise(models.Model):
+    workout = models.ForeignKey('Workout', on_delete=models.CASCADE)
+    exercise_type = models.ForeignKey('ExerciseType', on_delete=models.CASCADE)
     notes = models.TextField(blank=True)
 
     def __str__(self):
         return '{} -- {} -- {}'.format(self.lifter, self.exercise, self.lifted_date)
 
+class Set(models.Model):
+    exercise = models.ForeignKey('Exercise', on_delete=models.CASCADE, related_name='sets')
+    weight = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(1500)])
+    reps = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(50)])
 
-class Exercise(models.Model):
+class ExerciseType(models.Model):
     """Keep track of the supported exercises. Allows for generation of
         workouts and reporting on dem gainz"""
     name = models.CharField(max_length=50)
