98be0429658fba7881e43cc13898ae822761aa0d
Jason Hancock <jasonrhancock@gmail.com>
2018-10-09 20:05:14 -0500

    SETSS. Really? Reset DB, moved SetsInline around a little bit. Not much progress.

--- workouts/admin.py
+++ workouts/admin.py
@@ -1,22 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
-from workouts.models import Workout, Exercise, Set, ExerciseType
+from workouts.models import Workout, Exercise, Sets, ExerciseType
 ### PS: always better to use the full reference for import
 
-class SetInline(admin.TabularInline):
-    model = Set
+class SetsInline(admin.TabularInline):
+    model = Sets
 
 class ExerciseInline(admin.TabularInline):
     model = Exercise
-    inlines = [SetInline]
+    # inlines = [SetsInline]
 
 class WorkoutAdmin(admin.ModelAdmin):
     model = Workout
     fields = ('lifter',)
     readonly_fields = ('date',)
-    inlines = [ExerciseInline]
+    inlines = [ExerciseInline, SetsInline]
 
 admin.site.register(Workout, WorkoutAdmin)
-admin.site.register(Exercise)
 admin.site.register(ExerciseType)
--- workouts/models.py
+++ workouts/models.py
@@ -1,35 +1,40 @@
 from django.db import models
 from django.core.validators import MaxValueValidator
-from django import forms
 
 
-# Workout
-    # Exercise
-        # Set
 class Workout(models.Model):
     lifter = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='workouts')
     date = models.DateTimeField(blank=True, null=True, auto_now_add=True)
-    
-# Create your models here.
+
+    def __str__(self):
+        return '{} -- {}'.format(self.lifter, self.date)
+
+
 class Exercise(models.Model):
     workout = models.ForeignKey('Workout', on_delete=models.CASCADE)
     exercise_type = models.ForeignKey('ExerciseType', on_delete=models.CASCADE)
     notes = models.TextField(blank=True)
 
     def __str__(self):
-        return '{} -- {} -- {}'.format(self.lifter, self.exercise, self.lifted_date)
+        return '{}'.format(self.exercise_type)
 
-class Set(models.Model):
+
+class Sets(models.Model):
+    workout = models.ForeignKey('Workout', on_delete=models.CASCADE)
     exercise = models.ForeignKey('Exercise', on_delete=models.CASCADE, related_name='sets')
     weight = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(1500)])
     reps = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(50)])
 
+    def __str__(self):
+        return '{} -- {} -- {}'.format(self.exercise, self.weight, self.reps)
+
+
 class ExerciseType(models.Model):
     """Keep track of the supported exercises. Allows for generation of
         workouts and reporting on dem gainz"""
-    name = models.CharField(max_length=50)
+    name = models.CharField(default='None', max_length=50)
     # eventually the muscle_group can be its own model as well to further refine
-    muscle_group = models.CharField(max_length=90)
+    muscle_group = models.CharField(default='None', max_length=90)
 
     def __str__(self):
         return self.name
